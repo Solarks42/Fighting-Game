1dec5e7e57d69e4cb4af50fd6348587ccc554440
Solarks42 <phamhoangminhduc@gmail.com>
2025-08-06 00:06:23 +0200

    Starting attack moveset and hitboxes etc

--- Fighting-Game/Assets/PlayerMovement.cs
+++ Fighting-Game/Assets/PlayerMovement.cs
@@ -7,73 +7,23 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     public float jump;
     public bool isJumpingState;
+    public bool isAttackingState = false;
 
     public Rigidbody2D rb;
     public Animator Anim;
-    public SpriteRenderer SpriteRender; 
+    public SpriteRenderer SpriteRender;
 
-    private float Move;
-    private float currentHeight; 
-    private float previousHeight;
     
+    private float MoveX;
+    private float MoveY;
+    private float currentHeight;
+    private float previousHeight;
+
+
     void Start()
     {
     }
-
-    void Update()
-    {
-        //movement & jumping 
-        float Move = Input.GetAxisRaw("Horizontal");
-
-        rb.linearVelocity = new Vector2(speed * Move, rb.linearVelocity.y);
-
-        if (Input.GetButtonDown("Jump") && isJumpingState == false)
-        {
-            rb.AddForce(new Vector2(rb.linearVelocity.x, jump));
-
-        }
-
-
-        // running anim
-        if (Move != 0)
-        {
-            Anim.SetBool("IsRunning", true);
-        }
-        else
-        {
-            Anim.SetBool("IsRunning", false);
-        }
-        //character flipping 
-        if (Move < 0)
-        {
-            SpriteRender.flipX = true;
-        }
-        if (Move > 0)
-        {
-            SpriteRender.flipX = false;
-        }
-
-
-        //fall anim
-        float currentHeight = rb.linearVelocity.y;
-
-        // Check if the object reached max jump height and started falling
-        if (previousHeight > 0 && currentHeight <= 0) {
-            Debug.Log("Reached max jump height!");
-         
-            
-            Anim.SetBool("IsJumping", false);
-             
-            Anim.SetBool("IsFalling", true);
-            
-
-        }
-
-        previousHeight = currentHeight;
-
-
-    }
-    // floor collision
+      // floor collision
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Floor"))
@@ -92,8 +42,89 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    void Update()
+    {
 
+        float MoveX = Input.GetAxisRaw("HorizontalA");
+        float MoveY = Input.GetAxisRaw("VerticalA");
+
+         //movement & jumping 
+        rb.linearVelocity = new Vector2(speed * MoveX, rb.linearVelocity.y);
+
+        if (Input.GetButtonDown("JumpA") && isJumpingState == false)
+        {
+            rb.AddForce(new Vector2(rb.linearVelocity.x, jump));
+
+        }
+
+
+        // running anim
+        if (MoveX != 0)
+        {
+            Anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            Anim.SetBool("IsRunning", false);
+        }
+        //character flipping 
+        if (MoveX < 0)
+        {
+            SpriteRender.flipX = true;
+        }
+        if (MoveX > 0)
+        {
+            SpriteRender.flipX = false;
+        }
+
+
+        //fall anim
+        float currentHeight = rb.linearVelocity.y;
+
+        // Check if the object reached max jump height and started falling
+        if (previousHeight > 0 && currentHeight <= 0)
+        {
+            Debug.Log("Reached max jump height!");
+
+            Anim.SetBool("IsJumping", false);
+
+            Anim.SetBool("IsFalling", true);
+        }
+
+        previousHeight = currentHeight;
+
+        if (Input.GetButtonDown("BasicAttackA") && isJumpingState == false && isAttackingState == false)
+        {
+            Anim.SetBool("BasicAttackA", true);
+            isAttackingState = true;
+        }
+        else if (Input.GetButtonDown("SPAttackA") && isJumpingState == false && isAttackingState == false)
+        {
+            Anim.SetBool("SPAttackA", true);
+            isAttackingState = true;
+        }
+        else if (Input.GetButtonDown("BlockingA") && isJumpingState == false && isAttackingState == false)
+        {
+            Anim.SetBool("IsBlockingA", true);
+            isAttackingState = true;
+        }
+        
+        
+
+    }
+    public void EndAttack()
+    {
+        Anim.SetBool("IsAttacking1A", false);
+        Anim.SetBool("IsAttacking2A", false);
+        Anim.SetBool("IsAttacking3A", false);
+        isAttackingState = false;
+    }
+        
+       
+ 
     
+
+
 
 }
 
